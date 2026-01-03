4f70ce091186b7e55d46f28313e1bdf5aeeb8090
Tetiana Sobolieva <tetiana.sobolieva.s@gmail.com>
2026-01-03 18:12:37 +0200

    Removed unnesessary space

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -6,7 +6,7 @@ export const UserInfo = ({ user }) => {
   }
 
   return (
-    <a className="UserInfo" href={`mailto: ${user.email}`}>
+    <a className="UserInfo" href={`mailto:${user.email}`}>
       {user.name}
     </a>
   );
